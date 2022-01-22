d176fa7c741dc78983890c6e43b4367777afb2ca
Jonathan0 <johnlin989@gmail.com>
2022-01-21 22:21:27 -0500

    load truck data from sfgov, update readme

--- README.md
+++ README.md
@@ -1,12 +1,32 @@
 # Trucks
 Pick your favorite food trucks around you. 
 
+## How to
 1. pre-request: java11
 2. build project `./gradlew clean build`
 3. Run the TrucksApplication `./gradlew bootRun`
 4. Test a GET request at localhost:8080 `curl -v localhost:8080`
-5. Get All Trucks `curl -v localhost:8080/trucks`
-6. Get one Truck with id `curl -v localhost:8080/trucks/2`
-7. Update a Truck `curl -X PUT -v localhost:8080/trucks/2 -H 'Content-type:application/json' -d '{"dayOrder": 1, "dayOfWeekStr": "Monday", "starttime": "10AM", "endtime": "3PM"}'`
-8. Add a new Truck `curl -X POST -v localhost:8080/trucks -H 'Content-type:application/json' -d '{"permitLocation": "70 Hope Ave", "latitude": 42.368697328144826, "longitude": -71.24739426023218}'`
-9. Delete a Truck with id `curl -X DELETE -v localhost:8080/trucks/3`
+5. Get All Trucks
+```
+curl -v localhost:8080/trucks
+```
+6. Get one Truck with id 
+```
+curl -v localhost:8080/trucks/2
+```
+7. Update a Truck
+```
+curl -X PUT -v localhost:8080/trucks/2 -H 'Content-type:application/json' -d '{"dayOrder": 1, "dayOfWeekStr": "Monday", "starttime": "10AM", "endtime": "3PM"}'
+```
+8. Add a new Truck
+```
+curl -X POST -v localhost:8080/trucks -H 'Content-type:application/json' -d '{"permitLocation": "70 Hope Ave", "latitude": 42.368697328144826, "longitude": -71.24739426023218}'
+```
+9. Delete a Truck with id
+```
+curl -X DELETE -v localhost:8080/trucks/3
+```
+
+## Continue Improvement
+1. Loading all Trucks Data from the [sfgov](https://data.sfgov.org/resource/jjew-r69b.json) by the day of week (now default sunday)
+2. Continue improving the GetTrucks utils for more...
--- src/main/java/com/food/LoadDatabase.java
+++ src/main/java/com/food/LoadDatabase.java
@@ -1,10 +1,16 @@
 package com.food;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+import static com.utils.GetTrucks.getTrucks;
 
 @Configuration
 class LoadDatabase {
@@ -13,10 +19,24 @@ class LoadDatabase {
 
     @Bean
     CommandLineRunner initDatabase(TruckRepository repository) {
+        JSONArray trucksArray = null;
+        try {
+            trucksArray = getTrucks(0);
+            for (Object obj : trucksArray) {
+                JSONObject truck = (JSONObject) obj;
+                String permitLocation = truck.get("location").toString();
+                double latitude = Double.parseDouble(truck.get("latitude").toString());
+                double longitude = Double.parseDouble(truck.get("longitude").toString());
+                log.info("Preloading " + repository.save(new Truck(permitLocation, latitude, longitude)));
+            }
+        } catch (IOException e) {
+            log.error("Can not find trucks", e);
+        }
 
+        JSONArray finalTrucksArray = trucksArray;
         return args -> {
-            log.info("Preloading " + repository.save(new Truck("3750 18TH ST", 37.761398102006275, -122.427241066455551)));
-            log.info("Preloading " + repository.save(new Truck("505 HOWARD ST", 37.788387888445406, -122.396123930376618)));
+            log.info("Preloading " + finalTrucksArray.size() + " trucks completed!");
+            // log.info("Preloading " + repository.save(new Truck("505 HOWARD ST", 37.788387888445406, -122.396123930376618)));
         };
     }
 }
