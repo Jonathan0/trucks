67e1006236b67ac598b0805a2d3f45aac5bea4fa
Jonathan0 <johnlin989@gmail.com>
2022-01-21 21:47:14 -0500

    receiving trucks data from sfgov, also add simple test case

--- src/main/java/com/utils/GetTrucks.java
+++ src/main/java/com/utils/GetTrucks.java
@@ -1,5 +1,48 @@
 package com.utils;
 
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
 public class GetTrucks {
 
+    public static JSONArray getTrucks(int dayorder) throws IOException {
+        URL url = new URL(String.format("https://data.sfgov.org/resource/jjew-r69b.json?dayorder=%d", dayorder));
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+
+        int status = connection.getResponseCode();
+        JSONArray truck_array = null;
+        System.out.println(status);
+        if (status != 200) {
+            throw new RuntimeException("HttpResponseCode: " + status);
+        } else {
+            StringBuilder inline = new StringBuilder();
+            Scanner scanner = new Scanner(url.openStream());
+
+            //Write all the JSON data into a string using a scanner
+            while (scanner.hasNext()) {
+                inline.append(scanner.nextLine());
+            }
+
+            //Close the scanner
+            scanner.close();
+
+            //Using the JSON simple library parse the string into a json object
+            JSONParser parse = new JSONParser();
+            try {
+                truck_array = (JSONArray) parse.parse(inline.toString());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return truck_array;
+    }
 }
--- src/test/java/com/TrucksApplicationTests.java
+++ src/test/java/com/TrucksApplicationTests.java
@@ -1,13 +1,26 @@
 package com;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+import static com.utils.GetTrucks.getTrucks;
 
 @SpringBootTest
 class TrucksApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
+    @Test
+    public void getTrucksTest() throws IOException {
+        JSONArray res = getTrucks(0);
+        Assertions.assertEquals(82, res.size());
+        Assertions.assertEquals("0", ((JSONObject) res.get(0)).get("dayorder"));
+    }
 }
