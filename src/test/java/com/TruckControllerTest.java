04094f2b61d5424d55b44b11a9aa22c73790f3ea
Jonathan0 <johnlin989@gmail.com>
2022-01-23 11:03:22 -0500

    clena up

---
+++ src/test/java/com/TruckControllerTest.java
@@ -0,0 +1,8 @@
+package com;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(TruckRecordControllerTest.class)
+public class TruckRecordControllerTest {
+    
+}
