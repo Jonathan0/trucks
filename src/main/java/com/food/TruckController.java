e3f78dd90206169c8399ee149c7bc6089d3d5ee9
Jonathan0 <johnlin989@gmail.com>
2022-01-25 21:26:02 -0500

    cleanup imports

--- src/main/java/com/food/TruckController.java
+++ src/main/java/com/food/TruckController.java
@@ -1,7 +1,6 @@
 package com.food;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/com/food/TruckRepository.java
+++ src/main/java/com/food/TruckRepository.java
@@ -1,9 +1,6 @@
 package com.food;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface TruckRepository extends JpaRepository<Truck, Long> {
 }
