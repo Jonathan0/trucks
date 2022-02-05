a0e82b54b26f207b61c7b6251ddd07b8a78742f7
Jonathan0 <johnlin989@gmail.com>
2022-02-05 15:58:00 -0500

    fix sonarCloud issues

--- src/main/java/com/food/TruckErrorHandlerController.java
+++ src/main/java/com/food/TruckErrorHandlerController.java
@@ -2,13 +2,14 @@ package com.food;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TruckErrorHandlerController implements ErrorController{
 
-    @RequestMapping("/error")
+    @RequestMapping(path = "/error", method = RequestMethod.GET)
     @ResponseBody
     public String getErrorPath() {
         return "<center><h1>There is no Truck found !</h1></center>";
